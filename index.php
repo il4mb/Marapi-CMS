1fe50f4594667ae2d7952cef997505a3fcf0847d
ilham B <80049379+ilh4mb@users.noreply.github.com>
2022-10-17 20:44:35 +0700

    commit

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "hallo guys";
+
+use classes\UriManager;
+
+$list = scandir($_SERVER['DOCUMENT_ROOT']."/module/classes");
+foreach( $list AS $file ) {
+
+    if(is_file($_SERVER['DOCUMENT_ROOT']."/module/classes/".$file)) {
+
+        include_once $_SERVER['DOCUMENT_ROOT']."/module/classes/".$file;
+    }
+}
+
+$uriM = new UriManager();
+
+print_r($uriM->in_array());
--- module/classes/UriManager.php
+++ module/classes/UriManager.php
@@ -14,13 +14,14 @@ class UriManager {
         $this->path = $_SERVER['REQUEST_URI'];
         $exploded = explode("?", $this->path);
 
-        $path = array_key_exists(0, $exploded) ? $exploded[0] : null;
+        $this->path = array_key_exists(0, $exploded) ? $exploded[0] : null;
         $query = array_key_exists(1, $exploded) ? $exploded[1] : null;
     }
 
     public function in_array() {
 
-        return explode("/", $this->path);
+        
+        return array_slice(explode("/", $this->path), 1);
     }
 
 }
