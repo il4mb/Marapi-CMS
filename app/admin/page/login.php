f19bd8f3a5a00af625f1571e186e4ef37d3e2735
ilham B <80049379+ilh4mb@users.noreply.github.com>
2022-10-21 23:00:17 +0700

    create login page

--- app/admin/page/login.php
+++ app/admin/page/login.php
@@ -1,2 +1,28 @@
 <?php
-echo "login";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app/admin/asset/style.css">
+    <title>Login Marapi</title>
+</head>
+
+<body>
+    <div class="container align-center">
+
+        <img class="mrp-logo" src="/core/assets/svg/Marapi-logo.svg"/>
+        <h1>Login to panel</h1>
+        <form data-width="100">
+            <input type="email" placeholder="Enter email address" require />
+            <input type="password" placeholder="Enter password" require />
+
+        </form>
+    </div>
+</body>
+
+</html>
