6c060f07958a1c3076d076aa162368e1ebffaf53
ilham B <durianbohong@gmail.com>
2022-11-20 15:22:53 +0700

    modified:   module/classes/Connection.php

--- module/classes/Connection.php
+++ module/classes/Connection.php
@@ -1,24 +1,32 @@
 <?php
+
 namespace classes;
+
 use PDO, PDOException;
-class CONN {
+
+class CONN
+{
 
     public PDO $DB;
-    public function PDO() : PDO {
-        try {
+    public function PDO(): PDO
+    {
 
-            require_once $_SERVER['DOCUMENT_ROOT']."/core/connetion/PDO.php";
-            /**
-             * @var PDO $DB - Database connection
-             */
-            $this->DB = $DB;
-            return $this->DB;
+        if (is_file($_SERVER['DOCUMENT_ROOT'] . "/core/connetion/PDO.php")) {
+            try {
+                require_once $_SERVER['DOCUMENT_ROOT'] . "/core/connetion/PDO.php";
+                /**
+                 * @var PDO $DB - Database connection
+                 */
+                $this->DB = $DB;
+                return $this->DB;
+            } catch (PDOException $e) {
+
+                print $e;
+                exit;
+            }
+        } else {
             
-        
-        } catch (PDOException $e) {
-        
-            print $e;
-            exit;
+            header("Location: /mrp/install/");
         }
     }
 }
