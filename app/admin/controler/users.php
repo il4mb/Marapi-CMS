b3bbcd74dde64fb16d6c545eee55ceec23f8fb6b
ilham B <durianbohong@gmail.com>
2022-11-24 19:14:51 +0700

    Changes to be committed: 	modified:   app/admin/asset/css/color.css 	modified:   app/admin/asset/css/icon.css 	modified:   app/admin/asset/css/mrp.css 	new file:   app/admin/controler/html/usersFragment.html 	modified:   app/admin/controler/users.php 	new file:   app/plugin/text_editor/.reg 	modified:   conf-data.json

--- app/admin/controler/users.php
+++ app/admin/controler/users.php
@@ -14,28 +14,46 @@ $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $loged = $_COOKIE['user'];
 $ench = new ENCH($loged);
-$loged = $ench->decrypt();
+$loged = (array)$ench->decrypt();
 
 
+$html = file_get_contents(__DIR__."/html/usersFragment.html");
 
-$html = "<ul class='list-item'>";
-foreach ($result as $content) {
+$html .= "<ul class='list-item'>";
+foreach ($result as $user) {
 
-    $id = $content['id'];
-    $username = $content['username'];
-    $password = $content['password'];
-    $role = function() use ($content) {
+    $id = $user['id'];
+    $username = function () use ($user, $loged) {
 
+        if ($loged['id'] == $user['id']) {
+            return $user['username'] . " <i> ( You ) </i>";
+        } else return $user['username'];
+    };
+    $password = $user['password'];
+    $role = function () use ($user) {
         $value = "unknown";
-        switch($content['role']) {
+        switch ($user['role']) {
             case 1:
                 $value = "Admin";
                 break;
-            // ...
+                // ...
 
         }
-
         return $value;
+    };
+
+    $tool = function () use ($user, $loged) {
+
+        $html = "<a class='action-btn text-primary'>Edit</a>";
+
+        if ($loged['id'] !== $user['id']) {
+
+            if ($loged['role'] == 1) 
+                $html .= "<a trigger='delete' data-act='user' act-key='".$user['id']."' class='action-btn text-danger'>Delete</a>";
+
+        } else $html .= "<a class='text-danger action-btn'>Logout</a>";
+
+        return $html;
     };
 
 
@@ -44,18 +62,17 @@ foreach ($result as $content) {
         <div class='item-body flex align-items-center'>
 
             <i style='font-size: 35px;' class='micon-person-circle'></i>
-            <h4 class='item-title' style='margin: 0 10px;'>" . $username . "</h4>
+            <h4 class='item-title' style='margin: 0 10px;'>" . $username() . "</h4>
 
             <span>role : " . $role() . "</span>
 
-            <div style='width: 100%; padding: 8px;'>
-                <span>ID : $id</span>
+            <div style='width: 100%; padding: 15px 8px 0px 8px;' class='action-wrapper'>
+                " . $tool() . "
             </div>
            
         </div>
 
     </li>";
-
 }
 $html .= "</ul>";
 
